438fb81dfcb62947d87572545a00a232655a0e28
admin <282679878@qq.com>
2018-01-23 16:16:58 +0800

    bolang-clw

--- classes/artifacts/Logistics_war_exploded/js/wbb/cq/cqWin.js
+++ classes/artifacts/Logistics_war_exploded/js/wbb/cq/cqWin.js
@@ -4,132 +4,74 @@ Ext.cq.form = Ext.extend(Ext.FormPanel, {
 			constructor : function(app) {
 				this.app = app;
 
-				this.radio = new Ext.form.RadioGroup({
-							fieldLabel : '进出口',
-							items : [{
-										boxLabel : '进口',
-										name : 'type',
-										checked : true,
-										inputValue : 1,
-										listeners : {
-											check : function(checkbox, checked) {
-												if (checked) {
-												}
-											}
-										}
-									}, {
-										boxLabel : '出口',
-										inputValue : 2,
-										name : 'type',
-										listeners : {
-											check : function(checkbox, checked) {
-												if (checked) {
-												}
-											}
-										}
-									}],
-							scope : this
-						});
 
-				this.items = [{
-							xtype : 'hidden',
-							id : 'id'
-						}, {
-							xtype : 'hidden',
-							id : 'kqSortId'
-						}, {
-							xtype : 'hidden',
-							id : 'kqId'
-						}, {
-							xtype : 'hidden',
-							id : 'kqEvolveId'
-						}, {
-							xtype : 'hidden',
-							id : 'realFormula'
-						}, {
-							columnWidth : .8,
-							labelWidth : 60,
-							items : [{
-										fieldLabel : '结果指标',
-										xtype : 'textfield',
-										name : 'name',
-										submitValue : false,
-										anchor : '98%',
-										style : 'background:#E6E6E6',
-										readOnly : true,
-										// disabled : true,
-										allowBlank : false
-									}]
-						}, {
-							columnWidth : .2,
-							items : [{
-								xtype : 'button',
-								text : '引用',
-								iconCls : 'zoom',
-								anchor : '100%',
-								listeners : {
-									'click' : function() {
-										new kqSelector(function(ary) {
-													this
-															.getForm()
-															.findField('name')
-															.setValue(ary[0].chName);
-													this
-															.getForm()
-															.findField('kqId')
-															.setValue(ary[0].id);
-													this
-															.getForm()
-															.findField('kqSortId')
-															.setValue(ary[0].sortId);
-												}, true, this);
-									},
-									scope : this
-								}
-							}]
-						}, {
-							columnWidth : 1,
-							labelWidth : 60,
-							items : [this.radio]
-						}, {
-							columnWidth : .8,
-							labelWidth : 60,
-							items : [{
-										fieldLabel : '计算关系',
-										xtype : 'textfield',
-										name : 'showFormula',
-										readOnly : true,
-										style : 'background:#E6E6E6',
-										anchor : '98%'
-									}]
-						}, {
-							columnWidth : .2,
-							items : [{
-								xtype : 'button',
-								text : '配置',
-								iconCls : 'cog',
-								anchor : '100%',
-								listeners : {
-									'click' : function() {
-										this.kqId = this.getForm()
-												.findField('kqId').getValue();
-										this.type = this.radio.getValue().inputValue;
-										new calculator(function(showFormula,
-														realFormula) {
-													this
-															.getForm()
-															.findField('showFormula')
-															.setValue(showFormula);
-													this
-															.getForm()
-															.findField('realFormula')
-															.setValue(realFormula);
-												}, this, 'cq')
-									},
-									scope : this
-								}
-							}]
-						}];
+
+                this.userSelector = new Ext.form.TriggerField({
+                    fieldLabel : '用户',
+                    name : 'userName',
+                    anchor : '98%',
+                    triggerClass : 'x-form-search-trigger',
+                    selectOnFocus : true,
+                    submitValue : false,
+                    allowBlank : true,
+                    editable : false,
+                    onTriggerClick : function(e) {
+                        new userSelector(function(id, name,userFleetId) {
+                            this.setValue(name);
+                            Ext.getCmp('userId').setValue(id);
+                            basefleedId = userFleetId;
+
+
+
+
+                        }, true, this);
+                    },
+                    scope : this
+                });
+
+
+                this.kqSelector = new Ext.form.TriggerField({
+                    fieldLabel : '机构',
+                    name : 'company',
+                    anchor : '98%',
+                    triggerClass : 'x-form-search-trigger',
+                    selectOnFocus : true,
+                    submitValue : false,
+                    allowBlank : true,
+                    editable : false,
+                    onTriggerClick : function(e) {
+                        new kqSelector(function(id, name) {
+                            this.setValue(name);
+                            Ext.getCmp('companyId').setValue(id);
+
+
+
+
+                        }, true, this);
+                    },
+                    scope : this
+                });
+
+
+                this.items = [{
+                    xtype : 'hidden',
+                    id : 'id'
+                },{
+                    xtype : 'hidden',
+                    id : 'userId'
+                },{
+                    xtype : 'hidden',
+                    id : 'companyId'
+                }, {
+                    columnWidth : 1,
+                    items : [this.userSelector]
+                },{
+                    columnWidth : 1,
+                    items : [this.kqSelector]
+                }];
+
+
+
 
 				Ext.cq.form.superclass.constructor.call(this, {
 							region : 'center',
@@ -151,7 +93,7 @@ Ext.cq.win = Ext.extend(Ext.Window, {
 				this.app = app;
 				this.form = new Ext.cq.form(this);
 				Ext.cq.win.superclass.constructor.call(this, {
-							width : 500,
+							width : 300,
 							plain : true,
 							showLock : true,
 							modal : true,
@@ -171,20 +113,28 @@ Ext.cq.win = Ext.extend(Ext.Window, {
 									}]
 						});
 			},
-			onSave : function(btn) {
-				var form = this.form.getForm();
-				if (form.isValid()) {
-					btn.setDisabled(true);
-					var cq = form.getValues();
-					Ext.eu.ajax(path + '/wbb/saveCq.do', {
-								cq : Ext.encode(cq)
-							}, function(resp) {
-								Ext.ux.Toast.msg('信息', '保存成功');
-								this.app.getStore().reload();
-								this.close();
-							}, this);
-				}
-			},
+		onSave : function(btn) {
+        var form = this.form.getForm();
+        if (form.isValid()) {
+            btn.setDisabled(true);
+            var user = form.getValues();
+            console.log(user);
+            Ext.eu.ajax(path + '/logistics/saveapproveCompany.do', {
+
+                approveCompany : Ext.encode(user)
+            }, function(resp) {
+                var res = Ext.decode(resp.responseText);
+                if (res.label) {
+                    Ext.ux.Toast.msg('信息', '保存成功');
+                    this.app.getStore().reload();
+                    this.close();
+                } else {
+                    Ext.ux.Toast.msg('提示', '类型名称已经存在！！！');
+                    btn.setDisabled(false);
+                }
+            }, this);
+        }
+    },
 			onClose : function() {
 				this.close();
 			}
